ce86f45508ead56f43330adfbd0e98bf32ee1168
Екатерина <korobkova.ek99@mail.ru>
2020-04-06 18:14:29 +0300

    AlgebraPolinomion

---
+++ algebra_polynomial/base/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include "postfix.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	string expression;
+
+	double res;
+
+	cout << "Введите арифметическое выражение: ";
+	cin >> expression;
+	TPostfix postfix(expression);
+	cout << "Вы ввели aрифметическое выражение: " << postfix.GetInfix() << endl;
+	postfix.ToPostfix();
+	cout << "Постфиксная форма: " << postfix.GetPostfix() << endl;
+	res = postfix.Calculate();
+	cout << res << endl;
+
+	return 0;
+}
